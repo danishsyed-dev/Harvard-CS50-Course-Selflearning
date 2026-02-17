c1050a6a906f92b35e907c5bbe0ef49766674692
danishsyed-dev <danishsyed237@gmail.com>
2026-02-17 16:42:01 +0530

    Add CS50 input scaffolding to conditionals
    Add #include <cs50.h> and #include <stdio.h> and introduce a main() that prompts for int x and y via get_int in three WEEK-1 files (Conditional-2.c, Conditionals-0.c, Conditionals-1.c). This wires up user input for the example comparisons; note that the existing if statements remain outside main in these diffs and should be moved inside main to compile and run correctly.

--- WEEK-1/Conditionals-0.c
+++ WEEK-1/Conditionals-0.c
@@ -1,5 +1,13 @@
 // Conditionals that are mutually exclusive
 
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+}
 if (x < y)
 {
     printf("x is less than y\n");
--- WEEK-1/Conditionals-1.c
+++ WEEK-1/Conditionals-1.c
@@ -1,5 +1,12 @@
 // Conditional that isn't necessary
+#include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+}
 if (x < y)
 {
     printf("x is less than y\n");
